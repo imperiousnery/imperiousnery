addf6f815e3dd29559046b524f88a16b02df6626
Luan Nery <79871298+lu4nn3ry@users.noreply.github.com>
2022-12-04 13:42:45 -0300

    test

---
+++ lib/sections/main/main_section.dart
@@ -0,0 +1,53 @@
+import '/animations/entrance_fader.dart';
+import '/configs/app.dart';
+import '/configs/configs.dart';
+import '/provider/app_provider.dart';
+import '/provider/drawer_provider.dart';
+import '/provider/scroll_provider.dart';
+import '/responsive/responsive.dart';
+import '/utils/navbar_utils.dart';
+import '/utils/utils.dart';
+import '/widget/navbar_actions_button.dart';
+import '/widget/navbar_logo.dart';
+import 'package:provider/provider.dart';
+
+import 'package:universal_html/html.dart' as html;
+import 'package:flutter/material.dart';
+import '/constants.dart';
+import '/widget/arrow_on_top.dart';
+
+part 'widgets/_navbar_desktop.dart';
+part 'widgets/_mobile_drawer.dart';
+part 'widgets/_body.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    App.init(context);
+    final drawerProvider = Provider.of<DrawerProvider>(context);
+
+    return Scaffold(
+      key: drawerProvider.key,
+      extendBodyBehindAppBar: true,
+      drawer: !Responsive.isDesktop(context) ? const _MobileDrawer() : null,
+      body: SafeArea(
+        child: Stack(
+          children: [
+            const _Body(),
+            const ArrowOnTop(),
+            Responsive.isTablet(context) || Responsive.isMobile(context)
+                ? const _NavBarTablet()
+                : const _NavbarDesktop(),
+          ],
+        ),
+      ),
+    );
+  }
+}
